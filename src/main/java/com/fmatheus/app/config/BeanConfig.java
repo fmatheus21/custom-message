772c591fcba57aab6d436f648e7b5c8531557791
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-11-06 22:36:22 -0300

    Inclusão de bean.

---
+++ src/main/java/com/fmatheus/app/config/BeanConfig.java
@@ -0,0 +1,36 @@
+package com.fmatheus.app.config;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+
+import java.util.Properties;
+
+@Configuration
+public class BeanConfig {
+
+    public Properties yamlProperties() {
+        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
+        bean.setResources(new ClassPathResource("i18n/messages.yml"));
+        return bean.getObject();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setCommonMessages(yamlProperties());
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+}
