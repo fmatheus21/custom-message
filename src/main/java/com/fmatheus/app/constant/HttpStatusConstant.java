c0d40704431183d7f65e6dfe8fa961b7d1423c58
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-11-14 00:19:48 -0300

    Inclusão de mensagem.

--- src/main/java/com/fmatheus/app/constant/HttpStatusConstant.java
+++ src/main/java/com/fmatheus/app/constant/HttpStatusConstant.java
@@ -1,30 +1,26 @@
 package com.fmatheus.app.constant;
 
-import lombok.ToString;
-import org.springframework.http.HttpStatus;
-
-@ToString
 public class HttpStatusConstant {
 
     private HttpStatusConstant() {
         throw new IllegalStateException(getClass().getName());
     }
 
-    public static final String OK = HttpStatus.OK.getReasonPhrase().toString();
-    public static final String OK_NUMBER = String.valueOf(HttpStatus.OK.value()).toString();
-    public static final String CREATED = HttpStatus.CREATED.getReasonPhrase();
-    public static final String CREATED_NUMBER = String.valueOf(HttpStatus.CREATED.value());
-    public static final String FORBIDDEN = HttpStatus.FORBIDDEN.getReasonPhrase();
-    public static final String FORBIDDEN_NUMBER = String.valueOf(HttpStatus.FORBIDDEN.value());
-    public static final String INTERNAL_SERVER_ERROR = HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase();
-    public static final String INTERNAL_SERVER_ERROR_NUMBER = String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value());
-    public static final String NO_CONTENT = HttpStatus.NO_CONTENT.getReasonPhrase();
-    public static final String NO_CONTENT_NUMBER = String.valueOf(HttpStatus.NO_CONTENT.value());
-    public static final String UNAUTHORIZED = HttpStatus.UNAUTHORIZED.getReasonPhrase();
-    public static final String UNAUTHORIZED_NUMBER = String.valueOf(HttpStatus.UNAUTHORIZED.value());
-    public static final String BAD_REQUEST = HttpStatus.BAD_REQUEST.getReasonPhrase();
-    public static final String BAD_REQUEST_NUMBER = String.valueOf(HttpStatus.BAD_REQUEST.value());
-    public static final String NOT_FOUND = HttpStatus.NOT_FOUND.getReasonPhrase();
-    public static final String NOT_FOUND_NUMBER = String.valueOf(HttpStatus.NOT_FOUND.value());
+    public static final String OK = "Ok";
+    public static final String OK_NUMBER = "200";
+    public static final String CREATED = "Created";
+    public static final String CREATED_NUMBER = "201";
+    public static final String FORBIDDEN = "Forbidden";
+    public static final String FORBIDDEN_NUMBER = "403";
+    public static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+    public static final String INTERNAL_SERVER_ERROR_NUMBER = "500";
+    public static final String NO_CONTENT = "No Content";
+    public static final String NO_CONTENT_NUMBER = "204";
+    public static final String UNAUTHORIZED = "Unauthorized";
+    public static final String UNAUTHORIZED_NUMBER = "401";
+    public static final String BAD_REQUEST = "Bad Request";
+    public static final String BAD_REQUEST_NUMBER = "400";
+    public static final String NOT_FOUND = "Not Found";
+    public static final String NOT_FOUND_NUMBER = "404";
 
 }
